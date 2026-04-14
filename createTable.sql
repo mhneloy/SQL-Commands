6591124b1602252e1869c4f1592a7127b13a0fe2
mhneloy <mhneloy708@gmail.com>
2026-04-14 11:51:33 +0600

    rename & drop table command

--- createTable.sql
+++ createTable.sql
@@ -1,2 +1,11 @@
 use collage, ( select the database by comman or you can norammly select this by sidemenu bar)
 create table student (Roll int, Name varchar(20), GPA double(3,2), City varchar(15), primary key(Roll));
+
+
+
+=================== Rename Table =================
+RENAME TABLE student TO student_details;
+
+
+=================== Drop Table =================
+DROP TABLE student_details;
