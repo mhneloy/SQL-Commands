26905b373efa9b2c7a07eacbd3a6f77f1f235c83
mhneloy <mhneloy708@gmail.com>
2026-04-14 15:32:22 +0600

    DISTINCT & LIMIT command

--- createTable.sql
+++ createTable.sql
@@ -9,3 +9,11 @@ RENAME TABLE student TO student_details;
 
 =================== Drop Table =================
 DROP TABLE student_details;
+
+==================== Insert data into table ===============
+
+INSERT INTO student_details (Roll, Name, Gender, Age, City, GPA) VALUES (101, 'Rahim', 'Male', 18, 'Sylhet', 3.44) ;
+
+==================== Show data based on conditions ===============
+
+SELECT Roll, Name, Age, GPA FROM `student_details` WHERE GPA > 4;
---
+++ distinct-limit.sql
@@ -0,0 +1,14 @@
+
+=================== DISTINCT =====================
+********* remove the duplicate value **************
+
+
+
+SELECT DISTINCT City FROM `student_details` 
+
+
+=================== LIMIT =====================
+********* remove the duplicate value **************
+SELECT * FROM `student_details` LIMIT 5  // maximum koyta data show korbe
+
+SELECT * FROM `student_details` LIMIT 2,5 // koto theke suru kore maximum koyta data show korbe
