87dd1ee97a1874e4d13f76cc3d8253f78523de85
mhneloy <mhneloy708@gmail.com>
2026-04-14 11:40:27 +0600

    create table command

---
+++ createTable.sql
@@ -0,0 +1,2 @@
+use collage, ( select the database by comman or you can norammly select this by sidemenu bar)
+create table student (Roll int, Name varchar(20), GPA double(3,2), City varchar(15), primary key(Roll));
