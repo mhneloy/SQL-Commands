716c7f1c405cd3da2d4ff85b862a72885c4b2bef
mhneloy <mhneloy708@gmail.com>
2026-04-15 10:46:07 +0600

    IN & NOT IN use cases

---
+++ in-not_in.sql
@@ -0,0 +1,11 @@
+===================== IN =========================
+
+SELECT * FROM student_details 
+
+WHERE City IN ('Sylhet', 'Dhaka');
+
+
+===================== NOT IN =========================
+
+SELECT * FROM student_details
+WHERE City NOT IN ('Sylhet', 'Dhaka')
