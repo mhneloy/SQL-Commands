53b817874d6eb2fa36c59169af72d7de062983e5
mhneloy <mhneloy708@gmail.com>
2026-04-15 11:02:46 +0600

    like command use cases

---
+++ like.sql
@@ -0,0 +1,4 @@
+SELECT * FROM `student_details`
+WHERE Name LIKE '___i%' or Name LIKE 'h%' --protita dash ekta ekta character define kore, mane ___i mane 4 number character 'i' hote hobe er por jai thakuk problem nai"
+--'h%' mane first letter 'h' hote hobe pore ki thakuk matter na
+
